5fc27063073b6ee89ad0696be00b1629bdad6920
Matt Butterfield <matt.butterfield@percolate.com>
2015-03-21 19:24:28 -0400

    add random note button

--- PerfectPitch/ViewController.swift
+++ PerfectPitch/ViewController.swift
@@ -10,26 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var soundGenerator: SoundGenerator
+    let minNote = 60
+    let maxNote = 72
+    let velocity = UInt32(100)
+    let randomNoteFlag = UInt32(9999)
+
+    var soundGenerator = SoundGenerator()
+    var randomNote: UInt32
 
     required init(coder aDecoder: NSCoder) {
-        soundGenerator = SoundGenerator()
+        randomNote = UInt32(minNote)
         super.init(coder: aDecoder)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 
     @IBAction func playNoteOn(b:UIButton) {
         var note:UInt32 = UInt32(b.tag)
-        var velocity:UInt32 = 100
-        soundGenerator.playNoteOn(note, velocity: velocity)
+        if note == randomNoteFlag {
+            randomNote = generateRandomNote()
+            soundGenerator.playNoteOn(randomNote, velocity: velocity)
+        } else {
+            soundGenerator.playNoteOn(note, velocity: velocity)
+        }
     }
 
     @IBAction func playNoteOff(b:UIButton) {
         var note:UInt32 = UInt32(b.tag)
-        soundGenerator.playNoteOff(note)
+        if note == randomNoteFlag {
+            soundGenerator.playNoteOff(randomNote)
+        } else {
+            soundGenerator.playNoteOff(note)
+        }
     }
 
+    func generateRandomNote() -> UInt32 {
+        return minNote + UInt32(arc4random_uniform(UInt32(maxNote - minNote + 1)))
+    }
 }
